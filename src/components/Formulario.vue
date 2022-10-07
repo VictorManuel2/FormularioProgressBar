54cf3657e9b419a6bfd3dc90c5b2e7747f7b27cb
Victor <a3518110554@alumno.uttehuacan.edu.mx>
2022-10-07 00:43:45 -0500

    Clase 06 de sep

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,4 +1,51 @@
+<template>
+  <div class="row">
+    <div class="col-12 mb-4">
+        <progress-bar :porcentaje="porcentaje"/> 
+    </div>
+    <div class="col-12 col-md-4">
+      <form @submit.prevent="registrarProyecto">
+        <div class="mb-3">
+          <label class="form-label">Proyecto</label>
+          <input
+            v-model.trim="proyecto"
+            type="text"
+            class="form-control"
+            required
+          />
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Actividad</label>
+          <select v-model.trim="tipo" class="form-select" required>
+            <option disabled selected value="">
+              Selecciona un tipo de actividad
+            </option>
+            <option>Aplicaciones Web con Vue.js</option>
+            <option>Backend Services con Node</option>
+            <option>App móvil con React Native</option>
+          </select>
+        </div>
+
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-check-label"
+            >Urgente</label
+          >
+          <input v-model="urgente" type="checkbox" class="form-checkbox" />
+        </div>
+
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </form>
+    </div>
+    <div class="col-12 col-md-8">
+      <total-proyectos :numeroProyectos="numeroProyectos" :proyectos="proyectos" :cambiarEstado="cambiarEstado"/>
+    </div>
+  </div>
+</template>
+
 <script>
+  import ProgressBar from './ProgressBar.vue';
+  import TotalProyectos from './TotalProyectos.vue';
   export default {
     data: () => ({
       proyecto: "",
@@ -15,6 +62,7 @@
           completado: false,
         };
         this.proyectos.push(proyecto);
+        localStorage.setItem("proyectos", JSON.stringify(this.proyectos));
         
         this.proyecto = "";
         this.tipo = "";
@@ -31,89 +79,21 @@
       numeroProyectos () {
         return this.proyectos.length;
       },
-      porcemtaje (){
+      porcentaje (){
         let completados = 0;
         this.proyectos.map(proyecto => {
            if(proyecto.completado)
             completados++;
-        })
-        console.log((completados * 100) / this.numeroProyectos);
-        return;
+        });
+        return (completados * 100) / this.numeroProyectos || 0;
       },
     },
+    components:{ ProgressBar, TotalProyectos },
+    mounted (){
+      this.proyectos = JSON.parse(localStorage.getItem("proyectos")) || [];
+    }
   };
 </script>
-
-<template>
-<div class="row">
-  <div class="col-12 mb-4">
-    <h3 class="text-center">
-      Progreso 0%
-    </h3>
-    <div class="progress">
-      <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" 
-            aria-valuenow="25" 
-            aria-valuemin="0" 
-            aria-valuemax="100"
-            style="width: 50%" >
-
-      </div>  
-    </div>
-  </div>
-  <div class="col-12 col-md-4">
-    <form @submit.prevent="registrarProyecto">
-      <div class="mb-3">
-        <label class="form-label">Proyecto</label>
-        <input v-model.trim="proyecto" type="text" class="form-control" required> 
-      </div>
-    
-      <div class="mb-3">
-        <label class="form-label">Actividad</label>
-        <select v-model.trim="tipo" class="form-select" required>
-          <option disabled selected value="">Selecciona un tipo de actividad</option>
-          <option >Aplicaciones Web con Vue.js</option>
-          <option >Backend Services con Node</option>
-          <option >App móvil con React Native</option>
-        </select>
-      </div>
-    
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-check-label">Urgente</label> 
-        <input v-model="urgente" type="checkbox" class="form-checkbox" />
-      </div>
-     
-      <button type="submit" class="btn btn-primary">Guardar</button>
-    </form>
-  </div>
-
-<div class="col-12 col-md-8">
-  <h3>Total de Proyectos: {{numeroProyectos}}</h3>
-  <div class="table-responsive">
-      <table class="table table-dark table-hover">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Proyecto</th>
-            <th>Tipo</th>
-            <th>Urgente</th>
-            <th>Completado</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(proyecto, index) in proyectos" :key="index">
-            <td>{{ index + 1 }}</td>
-            <td>{{ proyecto.proyecto }}</td>
-            <td>{{ proyecto.tipo }}</td>
-            <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-danger' : 'bg-success'">{{proyecto.urgente ? "Si" : "No"}}</td>
-            <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'">{{proyecto.completado ? "Completado" : "Incompleto"}}</td>
-
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-</div>
-</template>
 <!-- 
 <script>
 export default {
